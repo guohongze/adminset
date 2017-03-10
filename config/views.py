afc769f8420f635dd640d37daea9228f456dbb5e
guohz <guohongze@126.com>
2017-03-10 13:21:03 +0800

    解决asset add 时提示问题 新增数据配置项目

--- config/views.py
+++ config/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 @login_required()
 def index(request):
     temp_name = "config/config-header.html"
+    display_control = "none"
     # dirs = os.path.split(os.path.realpath(__file__))[0]
     dirs = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     config = ConfigParser.ConfigParser()
@@ -18,6 +19,12 @@ def index(request):
         r_path = config.get('config', 'roles_path')
         p_path = config.get('config', 'playbook_path')
         s_path = config.get('config', 'scripts_path')
+        engine = config.get('db', 'engine')
+        host = config.get('db', 'host')
+        port = config.get('db', 'port')
+        user = config.get('db', 'user')
+        password = config.get('db', 'password')
+        database = config.get('db', 'database')
     return render_to_response('config/index.html', locals())
 
 
@@ -25,10 +32,19 @@ def index(request):
 def config_save(request):
     temp_name = "config/config-header.html"
     if request.method == 'POST':
+        # path
         ansible_path = request.POST.get('ansible_path')
         roles_path = request.POST.get('roles_path')
         pbook_path = request.POST.get('pbook_path')
         scripts_path = request.POST.get('scripts_path')
+        #db
+        engine = request.POST.get('engine')
+        host = request.POST.get('host')
+        port = request.POST.get('port')
+        user = request.POST.get('user')
+        password = request.POST.get('password')
+        database = request.POST.get('database')
+
         config = ConfigParser.RawConfigParser()
         dirs = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         config.add_section('config')
@@ -36,6 +52,15 @@ def config_save(request):
         config.set('config', 'roles_path', roles_path)
         config.set('config', 'playbook_path', pbook_path)
         config.set('config', 'scripts_path', scripts_path)
+        config.add_section('db')
+        config.set('db', 'engine', engine)
+        config.set('db', 'host', host)
+        config.set('db', 'port', port)
+        config.set('db', 'user', user)
+        config.set('db', 'password', password)
+        config.set('db', 'database', database)
+        tips = u"保存成功！"
+        display_control = ""
         with open(dirs+'/adminset.conf', 'wb') as cfgfile:
             config.write(cfgfile)
         with open(dirs+'/adminset.conf', 'r') as cfgfile:
@@ -44,7 +69,15 @@ def config_save(request):
             r_path = config.get('config', 'roles_path')
             p_path = config.get('config', 'playbook_path')
             s_path = config.get('config', 'scripts_path')
-        return render_to_response('config/index.html', locals())
+            engine = config.get('db', 'engine')
+            host = config.get('db', 'host')
+            port = config.get('db', 'port')
+            user = config.get('db', 'user')
+            password = config.get('db', 'password')
+            database = config.get('db', 'database')
+    else:
+        display_control = "none"
+    return render_to_response('config/index.html', locals())
 
 
 def get_dir(args):
