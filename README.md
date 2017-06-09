91036f566712ef972c31441e80d3a90a115b6bc2
guohongze <guohongze@126.com>
2017-06-09 19:49:19 +0800

    自动化安装优化

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ The open source operation platform : CMDB, project deploy, DevOps , Monitor. <br
 
 
 ## 服务端说明
-#### step1:准备
 建议将服务器端安装在centos7上，程序使用/opt/adminset目录（强制）<br>
 cd /opt<br>
 git clone https://github.com/guohongze/adminset.git<br>
--- install/server_install.sh
+++ install/server_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-adminset_dir="/opt/adminset/"
+adminset_dir="/opt/adminset"
 echo "####install depandencies####"
 yum install -y epel-release
 yum install -y make autoconf automake cmake gcc gcc-c++ 
@@ -23,18 +23,20 @@ case $db1 in
 		read -p "your database port:" db_port
 		read -p "your database user:" db_user
 		read -p "your database password:" db_password
-		mysql
+		[ ! $db_password ] && echo "your db_password is empty confirm please press Enter key"
+		[ -f /usr/bin/mysql ]
+		sleep 3
 		if [ $? -eq 0 ]
 		then
-			mysql -h$db_ip -P$dbport -u$db_user -p$db_password -e "CREATE DATABASE adminset DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+			mysql -h$db_ip -P$db_port -u$db_user -p$db_password -e "CREATE DATABASE adminset DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
 		else
 			yum install -y mysql
-			mysql -h$db_ip -P$dbport -u$db_user -p$db_password -e "CREATE DATABASE adminset DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+			mysql -h$db_ip -P$db_port -u$db_user -p$db_password -e "CREATE DATABASE adminset DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
 		fi
-		sed -i "s/host = 127.0.0.1/host = $db_ip/g" $adminset_dir/admiset.conf
-		sed -i "s/user = root/user = $db_user/g" $adminset_dir/admiset.conf
-		sed -i "s/port = 3306/port = $db_port/g" $adminset_dir/admiset.conf
-		sed -i "s/password =/password = $db_password/g" $adminset_dir/admiset.conf
+		sed -i "s/host = 127.0.0.1/host = $db_ip/g" $adminset_dir/adminset.conf
+		sed -i "s/user = root/user = $db_user/g" $adminset_dir/adminset.conf
+		sed -i "s/port = 3306/port = $db_port/g" $adminset_dir/adminset.conf
+		sed -i "s/password =/password = $db_password/g" $adminset_dir/adminset.conf
 		;;
 	*) 
 		exit 1                    
@@ -61,6 +63,7 @@ yum install nginx -y
 chkconfig nginx on
 scp $adminset_dir/install/adminset_nginx.conf /etc/nginx/conf.d
 scp $adminset_dir/install/nginx.conf /etc/nginx
+service nginx start
 nginx -s reload
 echo "##############install finished###################"
 service nginx start
