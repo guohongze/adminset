3b2de2eb1ec6c25b7dc6be16ca3a3de0112533b2
guohongze <guohongze@126.com>
2017-06-17 23:16:07 +0800

    echarts图表优化 轴提示

--- monitor/urls.py
+++ monitor/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='monitor'),
-    url(r'^host/info/(?P<hostname>\w+)/$', views.host_info, name='host_info'),
+    url(r'^system/$', views.index, name='monitor'),
+    url(r'^system/(?P<hostname>\w+)/$', views.host_info, name='host_info'),
     url(r'^get/data/(?P<hostname>\w+)/$', views.get_sys_data, name='get_sys_data'),
 ]
