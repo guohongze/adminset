52dd7e4a6c4c45ab14c838b6bb5818b2d3701591
guohongze <guohongze@126.com>
2017-06-23 17:51:38 +0800

    mongo认证后无法自动上报API问题处理

--- monitor/api.py
+++ monitor/api.py
@@ -9,24 +9,6 @@ import time
 from django.views.decorators.csrf import csrf_exempt
 
 
-@csrf_exempt
-@token_verify()
-def received_sys_info(request):
-    if request.method == 'POST':
-        mongodb_ip = get_dir("mongodb_ip")
-        mongodb_port = get_dir("mongodb_port")
-        received_json_data = json.loads(request.body)
-        hostname = received_json_data["hostname"]
-        received_json_data['timestamp'] = int(time.time())
-        client = MongoClient(mongodb_ip, int(mongodb_port))
-        db = client.sys_info
-        collection = db[hostname]
-        collection.insert_one(received_json_data)
-        return HttpResponse("Post the system Monitor Data successfully!")
-    else:
-        return HttpResponse("Your push have errors, Please Check your data!")
-
-
 class GetSysData(object):
     collection = get_dir("mongodb_collection")
 
@@ -57,3 +39,20 @@ class GetSysData(object):
         find_time = now_time-self.timing
         cursor = collection.find({'timestamp': {'$gte': find_time}}, {self.monitor_item: 1, "timestamp": 1}).limit(self.no)
         return cursor
+
+
+@csrf_exempt
+@token_verify()
+def received_sys_info(request):
+    if request.method == 'POST':
+        received_json_data = json.loads(request.body)
+        hostname = received_json_data["hostname"]
+        received_json_data['timestamp'] = int(time.time())
+        client = GetSysData.connect_db()
+        db = client[GetSysData.collection]
+        collection = db[hostname]
+        collection.insert_one(received_json_data)
+        return HttpResponse("Post the system Monitor Data successfully!")
+    else:
+        return HttpResponse("Your push have errors, Please Check your data!")
+
