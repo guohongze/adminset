99fc48b54402f0d409f86232690332facdd733bd
guohongze <guohongze@126.com>
2018-03-20 17:52:57 +0800

    显示标签修改

--- cmdb/asset.py
+++ cmdb/asset.py
@@ -98,7 +98,7 @@ def create_asset_excel(export, asset_id_all):
             file_name = 'adminset_cmdb_' + now + '.csv'
             response['Content-Disposition'] = "attachment; filename="+file_name
             writer = csv.writer(response)
-            writer.writerow([str2gb(u'主机名'), str2gb(u'IP地址'), str2gb(u'其它IP'), str2gb(u'主机组'),
+            writer.writerow([str2gb(u'主机名'), str2gb(u'IP地址'), str2gb(u'其它IP'), str2gb(u'所在机房'),
                              str2gb(u'资产编号'), str2gb(u'设备类型'), str2gb(u'设备状态'), str2gb(u'操作系统'),
                              str2gb(u'设备厂商'), str2gb(u'CPU型号'), str2gb(u'CPU核数'), str2gb(u'内存大小'),
                              str2gb(u'硬盘信息'), str2gb(u'SN号码'), str2gb(u'所在位置'),
@@ -114,7 +114,7 @@ def create_asset_excel(export, asset_id_all):
                     a_status = ASSET_STATUS[at_as-1][1]
                 else:
                     a_status = ""
-                writer.writerow([str2gb(h.hostname), h.ip, h.other_ip, str2gb(h.group), str2gb(h.asset_no),
+                writer.writerow([str2gb(h.hostname), h.ip, h.other_ip, str2gb(h.idc), str2gb(h.asset_no),
                                  str2gb(a_type), str2gb(a_status), str2gb(h.os), str2gb(h.vendor),
                                  str2gb(h.cpu_model), str2gb(h.cpu_num), str2gb(h.memory), str2gb(h.disk),
                                  str2gb(h.sn), str2gb(h.position), str2gb(h.memo)])
@@ -127,7 +127,7 @@ def create_asset_excel(export, asset_id_all):
         file_name = 'adminset_cmdb_' + now + '.csv'
         response['Content-Disposition'] = "attachment; filename=" + file_name
         writer = csv.writer(response)
-        writer.writerow([str2gb('主机名'), str2gb('IP地址'), str2gb('其它IP'), str2gb('主机组'), str2gb('资产编号'),
+        writer.writerow([str2gb('主机名'), str2gb('IP地址'), str2gb('其它IP'), str2gb('所在机房'), str2gb('资产编号'),
                          str2gb('设备类型'), str2gb('设备状态'), str2gb('操作系统'), str2gb('设备厂商'), str2gb('CPU型号'),
                          str2gb('CPU核数'), str2gb('内存大小'), str2gb('硬盘信息'), str2gb('SN号码'),
                          str2gb('所在位置'), str2gb('备注信息')])
@@ -142,7 +142,7 @@ def create_asset_excel(export, asset_id_all):
                 a_status = ASSET_STATUS[at_as-1][1]
             else:
                 a_status = ""
-            writer.writerow([str2gb(h.hostname), h.ip, h.other_ip, str2gb(h.group), str2gb(h.asset_no), str2gb(a_type),
+            writer.writerow([str2gb(h.hostname), h.ip, h.other_ip, str2gb(h.idc), str2gb(h.asset_no), str2gb(a_type),
                              str2gb(a_status), str2gb(h.os), str2gb(h.vendor), str2gb(h.cpu_model), str2gb(h.cpu_num),
                              str2gb(h.memory), str2gb(h.disk), str2gb(h.sn), str2gb(h.position),
                              str2gb(h.memo)])
