bbd81b27daf3c86507ce15f1d601ef3a71544f03
guohongze <guohongze@126.com>
2018-04-08 21:36:24 +0800

    up.sh update build

--- install/server/up.sh
+++ install/server/up.sh
@@ -11,6 +11,18 @@ cur_dir=$(pwd)
 cd $adminset_dir
 pip install -r requirements.txt
 rsync --progress -ra --delete --exclude '.git' $cur_dir/ $adminset_dir
+
+# build webssh
+echo "build webssh"
+/usr/bin/yum install -y nodejs
+cd $cur_dir/vendor/WebSSH2
+#/usr/bin/npm install -g cnpm --registry=https://registry.npm.taobao.org
+#/usr/bin/cnpm install --production
+#/usr/bin/cnpm install forever -g
+/usr/bin/npm config set registry http://registry.cnpmjs.org
+/usr/bin/npm install --production
+/usr/bin/npm install forever -g
+
 cd $adminset_dir
 if [ $1 ]
 then
