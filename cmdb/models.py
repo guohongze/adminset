ada32e6684bebb5efd82bbca8385eaff0ca2fd73
wellfulren <1305627792@qq.com>
2018-09-01 08:56:11 +0800

    解除强依赖主机名，改成依赖IP

--- cmdb/models.py
+++ cmdb/models.py
@@ -51,8 +51,8 @@ class Idc(models.Model):
 
 
 class Host(models.Model):
-    hostname = models.CharField(max_length=50, verbose_name=u"主机名", unique=True)
-    ip = models.GenericIPAddressField(u"管理IP", max_length=15)
+    hostname = models.CharField(max_length=50, verbose_name=u"主机名")
+    ip = models.GenericIPAddressField(u"管理IP", max_length=15, unique=True)
     idc = models.ForeignKey(Idc, verbose_name=u"所在机房", on_delete=models.SET_NULL, null=True, blank=True)
     other_ip = models.CharField(u"其它IP", max_length=100, blank=True)
     asset_no = models.CharField(u"资产编号", max_length=50, blank=True)
