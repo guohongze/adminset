67cb98088c2e2e768c37d30be372da9382d07bf2
guohongze <guohongze@126.com>
2018-10-18 17:49:24 +0800

    hotfix mfile permission

--- mfile/views.py
+++ mfile/views.py
@@ -1,9 +1,12 @@
 from django.views.generic.base import View
 from django.shortcuts import render_to_response
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.decorators import  method_decorator
+from accounts.permission import permission_verify
 
 
 class finder(LoginRequiredMixin,View):
+    @method_decorator(permission_verify())
     def get(self,request):
         temp_name = "mfile/mfile-header.html"
         return render_to_response('mfile/finder.html', locals())
