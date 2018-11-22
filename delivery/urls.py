bdc6669318800e3d7436098287c14c6e0dfeb723
guohongze <guohongze@126.com>
2018-11-22 17:44:33 +0800

    deploy logs display

--- delivery/urls.py
+++ delivery/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^edit/(?P<project_id>\d+)/$', deli.delivery_edit, name='delivery_edit'),
     url(r'^log/(?P<project_id>\d+)/$', deli.log, name='delivery_log'),
     url(r'^log2/(?P<project_id>\d+)/$', deli.log2, name='delivery_log2'),
+    url(r'^logs_history/(?P<project_id>\d+)/$', deli.logs_history, name='logs_history'),
     url(r'^deploy/(?P<project_id>\d+)/$', deli.delivery_deploy, name='delivery_deploy'),
     url(r'^taskstop/(?P<project_id>\d+)/$', deli.task_stop, name='delivery_taskstop'),
     url(r'^delete/$', deli.delivery_del, name='delivery_del'),
