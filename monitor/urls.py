6cfaec8e611c5b526c924ba3b7cab0265cdad778
guohongze <guohongze@126.com>
2019-02-12 17:46:51 +0800

    fix monitor received

--- monitor/urls.py
+++ monitor/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     url(r'^getmem/(?P<hostname>.+)/(?P<timing>\d+)/$', system.get_mem, name='get_mem'),
     url(r'^getdisk/(?P<hostname>.+)/(?P<timing>\d+)/(?P<partition>\d+)/$', system.get_disk, name='get_disk'),
     url(r'^getnet/(?P<hostname>.+)/(?P<timing>\d+)/(?P<net_id>\d+)/$', system.get_net, name='get_net'),
-    url(r'^receivedsysinfo/$', api.received_sys_info, name='received_sys_info'),
+    url(r'^received/sys/info/$', api.received_sys_info, name='received_sys_info'),
 ]
